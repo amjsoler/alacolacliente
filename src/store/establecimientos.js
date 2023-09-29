53215e543982000282fd4d7fe528d38716571db2
Jorge Amat Soler <amjsoler@gmail.com>
2023-09-30 01:11:31 +0200

    Modularizado el store de establecimientos

---
+++ src/store/establecimientos.js
@@ -0,0 +1,27 @@
+import axios from "axios";
+
+export default {
+    state: {
+        busquedaRealizada: []
+    },
+    mutations: {
+        buscarEstablecimientosMut(state, data){
+            console.log("Entro al mut con: " + data)
+            state.busquedaRealizada = data;
+        }
+    },
+    actions: {
+        async buscarEstablecimientos({commit}, busqueda) {
+            const data = await axios.get(
+                "http://localhost:8000/api/buscar-establecimientos",
+                {
+                    params: {
+                        "busqueda": busqueda
+                    }
+                }
+            )
+
+            commit("buscarEstablecimientosMut", data.data);
+        }
+    },
+}
--- src/store/index.js
+++ src/store/index.js
@@ -1,30 +1,14 @@
 import {createStore} from 'vuex'
-import axios from "axios";
+
+import establecimientos from "@/store/establecimientos";
 
 export default createStore({
     state: {
-        busquedaRealizada: []
     },
     getters: {},
-    mutations: {
-        buscarEstablecimientosMut(state, data){
-            console.log("Entro al mut con: " + data)
-            state.busquedaRealizada = data;
-        }
-    },
-    actions: {
-        async buscarEstablecimientos({commit}, busqueda) {
-            const data = await axios.get(
-                "http://localhost:8000/api/buscar-establecimientos",
-                {
-                    params: {
-                        "busqueda": busqueda
-                    }
-                }
-            )
-
-            commit("buscarEstablecimientosMut", data.data);
-        }
-    },
-    modules: {}
+    mutations: {},
+    actions: {},
+    modules: {
+        establecimientos
+    }
 })
