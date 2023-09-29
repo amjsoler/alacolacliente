bf815695a2a863f09bd026f4a52918e88a9f8748
Jorge Amat Soler <amjsoler@gmail.com>
2023-09-30 00:59:11 +0200

    Busqueda por cadena implementada

--- src/views/BuscarEstablecimiento.vue
+++ src/views/BuscarEstablecimiento.vue
@@ -1,9 +1,36 @@
 <template>
-    Buscar establecimiento
+    <form class="d-flex flex-row">
+        <div class="flex-fill">
+            <input v-model="busqueda" type="text" class="form-control">
+        </div>
+        <button @click.prevent="realizarBusqueda" class="btn btn-primary ms-3">
+            <span class="material-symbols-outlined icono">search</span>
+        </button>
+    </form>
+
 </template>
 
 <script>
+    import axios from "axios";
+
     export default {
-        name: "BuscarEstablecimiento"
+        name: "BuscarEstablecimiento",
+        data() {
+            return {
+                busqueda: "",
+            }
+        },
+        methods: {
+            realizarBusqueda: function () {
+                axios.get(
+                    "http://localhost:8000/api/buscar-establecimientos",
+                    {
+                        params: {
+                            "busqueda": this.busqueda
+                        }
+                    }
+                )
+            }
+        }
     }
 </script>
